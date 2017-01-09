a9dbdfb9cd666066aed549b60a8900cfa8917cf7
Chris Nelson <github@cryptzero.net>
2017-01-09 13:37:18 -0700

    modified path for environmental variables

--- gitall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sdlc/environment-variables
-
-GIT_REPOS=`curl https://api.github.com/orgs/${ORG_NAME}/repos?access_token=${ACCESS_TOKEN} | grep ssh_url | awk -F':' '{print $2,$3}' | sed 's/\ /\:/g' | sed 's/\:\"//g' | sed 's/\"\,//g'
-`
-for REPO in $GIT_REPOS; do
-git clone $REPO
-done
--- gitclone.sh
+++ gitclone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #run environment variables file with your org name and access token variables
-sdlc/environment-variables
+./environment-variables
 
 #pull list of organization repos
 GIT_REPOS=`curl https://api.github.com/orgs/${ORG_NAME}/repos?access_token=${ACCESS_TOKEN}| grep ssh_url | awk -F':' '{print $2,$3}' | sed 's/\ /\:/g' | sed 's/\:\"//g' | sed 's/\"\,//g'`
